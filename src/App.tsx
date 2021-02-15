740c48a4037925a4945442bc342b184c4d01e00a
Laurence Billingham <laurence@travelnest.com>
2021-02-15 21:55:01 +0000

    :sparkles: Game of Life with working gliders, toads, blinkers etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { produce } from 'immer';
 
 const numRows = 25;
@@ -7,6 +7,18 @@ const cellSize = 20;
 const borderStyle = "solid 1px black";
 const aliveColor = 'pink';
 const deadColor = undefined;
+const timeStep = 250; // ms
+
+const neighbourOffsets = [
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [1, 1],
+  [1, -1],
+  [-1, 0],
+  [-1, 1],
+  [-1, -1],
+];
 
 const filledGrid = (fillVal?: boolean) => {
   const filling = fillVal ? fillVal : false;
@@ -22,9 +34,47 @@ const App: React.FC = () => {
     return filledGrid();
   });
   const [running, setRunning] = useState(false)
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+    setGrid((currentGrid) => {
+      return produce(currentGrid, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbours = 0;
+            neighbourOffsets.forEach(([dx, dy]) => {
+              const newI = i + dx;
+              const newK = k + dy;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbours += currentGrid[newI][newK] ? 1 : 0;
+              }
+            }
+            )
+            if (neighbours < 2 || neighbours > 3) {
+              gridCopy[i][k] = false;
+            } else if (currentGrid[i][k] === false && neighbours === 3) {
+              gridCopy[i][k] = true;
+            }
+          }
+        }
+      });
+    });
+    setTimeout(runSimulation, timeStep)
+  }, [])
   return (
     <>
-      <button onClick={() => { setRunning(!running) }}>{running ? 'Stop' : 'Start'}</button>
+      <button onClick={() => {
+        setRunning(!running);
+        if (!running) {
+          runningRef.current = true;
+          runSimulation();
+        }
+      }}>{running ? 'Stop' : 'Start'}</button>
       <div
         style={{
           display: "grid",
